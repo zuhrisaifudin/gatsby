f3725b0777e9d9746539922162fc0572c66845e0
Matt Kane <matt@gatsbyjs.com>
2020-11-30 11:30:02 +0000

    Coerce numeric values to strings

--- packages/gatsby-plugin-image/src/babel-helpers.ts
+++ packages/gatsby-plugin-image/src/babel-helpers.ts
@@ -22,12 +22,31 @@ export const SHARP_ATTRIBUTES = new Set([
   `background`,
 ])
 
+const numericValues = [`width`, `height`, `maxWidth`, `maxHeight`, `quality`]
+
+function coerceValues(attrs: Record<string, unknown>): Record<string, unknown> {
+  numericValues.forEach((key: string) => {
+    if (typeof attrs[key] === `string`) {
+      const parsed = parseInt(attrs[key] as string, 10)
+      if (isNaN(parsed as number)) {
+        console.error(
+          `[gatsby-plugin-image] Invalid value "${attrs[key]}" provided for ${key} of StaticImage "${attrs.src}". Value must be numeric.`
+        )
+      } else {
+        attrs[key] = parsed
+      }
+    }
+  })
+  return attrs
+}
+
 export function evaluateImageAttributes(
   nodePath: NodePath<JSXOpeningElement>,
   onError?: (prop: string) => void
 ): Record<string, unknown> {
   // Only get attributes that we need for generating the images
-  return getAttributeValues(nodePath, onError, SHARP_ATTRIBUTES)
+  const attrValues = getAttributeValues(nodePath, onError, SHARP_ATTRIBUTES)
+  return coerceValues(attrValues)
 }
 
 export function hashOptions(options: unknown): string {
