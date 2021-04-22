ac0e18e7ba3f0244d4f223caab31d3bbb2e079a3
Ward Peeters <ward@coding-tech.com>
2021-04-22 12:11:34 +0200

    update script

--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -6,14 +6,21 @@ CUSTOM_COMMAND="${2:-yarn test}"
 GATSBY_PATH="${CIRCLE_WORKING_DIRECTORY:-../../}"
 TMP_LOCATION=$(mktemp -d);
 
+mkdir -p $TMP_LOCATION/$SRC_PATH
+TMP_TEST_LOCATION=$TMP_LOCATION/$SRC_PATH
+
+mkdir -p $TMP_LOCATION/scripts/
+mkdir -p $TMP_TEST_LOCATION
+
 # cypress docker does not support sudo and does not need it, but the default node executor does
 command -v gatsby-dev || command -v sudo && (sudo npm install -g gatsby-dev-cli@next) || npm install -g gatsby-dev-cli@next
 
 echo "Copy $SRC_PATH into $TMP_LOCATION to isolate test"
-cp -Rv $SRC_PATH/** $TMP_LOCATION
+cp -Rv $SRC_PATH/** $TMP_TEST_LOCATION
+cp -Rv $GATSBY_PATH/scripts/** $TMP_LOCATION/scripts/
 
 # setting up child integration test link to gatsby packages
-cd "$TMP_LOCATION"
+cd "$TMP_TEST_LOCATION"
 gatsby-dev --set-path-to-repo "$GATSBY_PATH"
 gatsby-dev --force-install --scan-once  # install _all_ files in gatsby/packages
 if test -f "./node_modules/.bin/gatsby"; then
